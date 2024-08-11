a76c38d6a08596c64f826ff7d26e521fec6deeba
HeinoLouwrens <73063671+HeinoLouwrens@users.noreply.github.com>
2024-08-11 18:39:15 +0200

    fix

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Form submission handling
     const form = document.querySelector('.contact-form form');
     form.addEventListener('submit', function (e) {
         e.preventDefault();
